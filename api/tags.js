33153e4bcf26813f291b5efdfeb6fb4d3e1ae7fc
buczekbrittany <bbucze2@uic.edu>
2023-01-20 20:47:57 -0800

    writing

--- api/tags.js
+++ api/tags.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const tagsRouter = express.Router();
 
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => {
     console.log("A request is being made to /tags");
@@ -16,6 +16,26 @@ tagsRouter.get('/', async (req, res) => {
         tags
     });
 });
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    // read the tagname from the params
+    const {tagName} = req.params;
+    try {
+      // use our method to get posts by tag name from the db
+      const getPosts = await getPostsByTagName(tagName);
+      const posts = getPosts.filter(post => {
+        return post.active || (req.user && post.author.id === req.user.id);})
+      // send out an object to the client { posts: // the posts }
+    if (posts){
+        res.send({
+            posts
+        });
+     }
+    } catch ({ name, message }) {
+      // forward the name and message to the error handler
+      next({ name, message })
+    }
+  });
 
 
 module.exports = tagsRouter;
--- db/index.js
+++ db/index.js
@@ -260,26 +260,36 @@ async function addTagsToPost(postId, tagList) {
   }
 }
 
+
 async function getPostById(postId) {
   try {
-    const { rows: [post] } = await client.query(`
-        SELECT *
-        FROM posts
-        WHERE id=$1;
-      `, [postId]);
+    const { rows: [ post ]  } = await client.query(`
+      SELECT *
+      FROM posts
+      WHERE id=$1;
+    `, [postId]);
+
+    // THIS IS NEW
+    if (!post) {
+      throw {
+        name: "PostNotFoundError",
+        message: "Could not find a post with that postId"
+      };
+    }
+    // NEWNESS ENDS HERE
 
     const { rows: tags } = await client.query(`
-        SELECT tags.*
-        FROM tags
-        JOIN post_tags ON tags.id=post_tags."tagId"
-        WHERE post_tags."postId"=$1;
-      `, [postId])
+      SELECT tags.*
+      FROM tags
+      JOIN post_tags ON tags.id=post_tags."tagId"
+      WHERE post_tags."postId"=$1;
+    `, [postId])
 
     const { rows: [author] } = await client.query(`
-        SELECT id, username, name, location
-        FROM users
-        WHERE id=$1;
-      `, [post.authorId])
+      SELECT id, username, name, location
+      FROM users
+      WHERE id=$1;
+    `, [post.authorId])
 
     post.tags = tags;
     post.author = author;
